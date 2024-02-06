282302a8c60a4bf6b72322b8421704e88aa6384a
FacundoCGM <114027262+FacundoCGM@users.noreply.github.com>
2024-02-06 13:40:34 -0300

    Entregable 11
    Se agrega el manejo de errores y el mocking products

---
+++ src/controller/ticketController.js
@@ -0,0 +1,23 @@
+import TicketService from "../service/ticketService.js"
+import Controllers from "./classController.js"
+
+const ticketService = new TicketService()
+
+export default class TicketController extends Controllers {
+    constructor() {
+        super(ticketService)
+    }
+
+    async generateTicket(req, res, next) {
+        try {
+            const { uid } = req.params
+            const { cid } = req.params
+
+            const ticket = await ticketService.generateTicket(uid, cid)
+            if(!ticket) res.status(404).json({msg: 'Error generate ticket'});
+            else res.status(200).json(ticket)
+        } catch(error) {
+            next(error)
+        }
+    }
+}
